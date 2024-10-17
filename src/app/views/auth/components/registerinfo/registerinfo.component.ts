c4d69f3d96879eb04867f1591ac6e421b134e5c9
Daniel Marinho <danielmarinho8@hotmail.com>
2024-10-17 09:53:28 -0300

    add register page

--- src/app/views/auth/components/registerinfo/registerinfo.component.ts
+++ src/app/views/auth/components/registerinfo/registerinfo.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../../../components/input/input.component';
+import { ButtonComponent } from '../../../../components/button/button.component';
 
 @Component({
   selector: 'app-registerinfo',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './registerinfo.component.html',
   styleUrl: './registerinfo.component.scss'
 })
--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthcardComponent } from '../components/authcard/authcard.component';
+import { RegisterinfoComponent } from '../components/registerinfo/registerinfo.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [AuthcardComponent, RegisterinfoComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
