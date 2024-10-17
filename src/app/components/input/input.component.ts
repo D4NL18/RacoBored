bba484d978dcaea9762dcc31da73bcbb510d5403
Daniel Marinho <danielmarinho8@hotmail.com>
2024-10-17 09:37:13 -0300

    add button and fix input

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,12 +1,9 @@
 import { Component, Input } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule],
+  imports: [],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
